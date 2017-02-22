8d676a76794a8ba8300ba4038ccf7e94d4a12235
zhanyangch <zhanyangch@gmail.com>
2017-02-22 21:05:24 +0800

    1.change my github ID from a91004bcd to zhanyangch 2.optimized by reduce struct memory 3.plot will print origin and opt time

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -5,11 +5,19 @@
 /* FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
-    /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
-    return NULL;
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+    return e;
 }
