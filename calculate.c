cdea66ae1a904744add9e1254f3f1fdfbf8a379c
zhanyangch <zhanyangch@gmail.com>
2017-02-26 10:07:53 +0800

    time of findName is displayed with ninth decimal place

--- calculate.c
+++ calculate.c
@@ -65,7 +65,7 @@ int main(void)
         hash_sum_f += hash_f;
     }
     fprintf(output, "append() %lf %lf %lf\n",orig_sum_a / 100.0, opt_sum_a / 100.0, hash_sum_a / 100.0);
-    fprintf(output, "findName() %lf %lf %lf", orig_sum_f / 100.0, opt_sum_f / 100.0, hash_sum_f / 100.0);
+    fprintf(output, "findName() %.9lf %.9lf %.9lf", orig_sum_f / 100.0, opt_sum_f / 100.0, hash_sum_f / 100.0);
     fclose(output);
     fclose(fp);
     return 0;
